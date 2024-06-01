e1e20ecb541190eaea4e5cc464ad9e6865c7d282
Rizki Pangestu <bangtutorial6528@gmail.com>
2024-06-01 22:06:54 +0700

    ubah code

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const logo1 = document.getElementById("logo1");
+  const logo2 = document.getElementById("logo2");
+
+  // Start the animation by adding the class
+  logo1.classList.add("animate-logo1");
+  logo2.classList.add("animate-logo2");
+});
